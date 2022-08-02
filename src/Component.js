f1462a9a87b0664a49fd269419bd3fa55f5388ca
Asma <asmabenarbia2@gmail.com>
2022-08-02 15:33:42 +0100

    update

---
+++ src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Component() {
+    return ( 
+        <div >
+               <h1> hello asma </h1>
+          
+        </div>
+    )
+}
+
+export default Component
